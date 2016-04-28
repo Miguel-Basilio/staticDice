6259caa5a582eb0e6ea53e8557bd622924a88ae8
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-04-27 21:32:05 -0700

    int Dices , int bounces

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 
+
 public class Main extends randomnumber{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(randomnumber.OneThrow(3, 7));
 		System.out.println(randomnumber.OneThrow());
 	}
 
--- src/randomnumber.java
+++ src/randomnumber.java
@@ -2,8 +2,15 @@
 public class randomnumber {
 
 	private static int randomnumber;
-	public static int OneThrow(){
-		 randomnumber = 1+(int )(Math.random()*6);
-			return randomnumber;
+
+	public static int OneThrow(int dices, int bounces){
+	
+	
+			randomnumber = 1+(int )(Math.random()*6);
+			return randomnumber;			
 }
+	public static int OneThrow(){
+		randomnumber = 1+(int )(Math.random()*6);
+		return randomnumber;
+	}
 }
