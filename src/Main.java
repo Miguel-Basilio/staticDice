337a74336b75c3559751e10e54798f00e6930768
MiguelBasilio <lokoa@DESKTOP-F4BVPJT>
2016-04-27 21:14:39 -0700

    static method

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main extends randomnumber{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(randomnumber.OneThrow());
+	}
+
+}
---
+++ src/randomnumber.java
@@ -0,0 +1,9 @@
+
+public class randomnumber {
+
+	private static int randomnumber;
+	public static int OneThrow(){
+		 randomnumber = 1+(int )(Math.random()*6);
+			return randomnumber;
+}
+}
