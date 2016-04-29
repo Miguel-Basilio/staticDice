758e480177e7c0c636ea4a5fc07edbf766977c78
Miguel-Basilio <MiguelBSAC@gmail.com>
2016-04-28 20:55:33 -0700

    Update randomnumber.java

--- src/randomnumber.java
+++ src/randomnumber.java
@@ -6,8 +6,8 @@ public class randomnumber {
 	public static int OneThrow(int dices, int bounces){
 	
 	
-			randomnumber = 1+(int )(Math.random()*6);
-			return randomnumber, dices , bounces;			
+		int randomNumber = (dices*(1+(int)(Math.random()*6)))+bounces;
+		return randNum;			
 }
 	public static int OneThrow(){
 		randomnumber = 1+(int )(Math.random()*6);
